d59b9a314c681c29ba064e03356c8ede9f8fc4ff
deadlygreenz <73725516+deadlygreenz@users.noreply.github.com>
2021-01-03 20:58:22 -0500

    Add files via upload

---
+++ bin.js
@@ -0,0 +1,22 @@
+class Bin {
+    constructor(x, y, width, height) {
+      var options = {
+          isStatic:true
+      }
+      this.image = loadImage("bin.png")
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      
+      
+      imageMode(CENTER);
+      fill("red");
+      image(this.image, pos.x,pos.y, this.width, this.height);
+      pop();
+    }
+  };
